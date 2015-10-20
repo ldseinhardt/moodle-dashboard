ec73956a9c7892a83b0db9726e2d4aebe021f83f
Luan Einhardt <ldseinhardt@gmail.com>
2015-10-20 22:31:18 +0000

    remodelado o sistema de páginas

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,20 +1,73 @@
-(function(chrome, $, mdash) {
+(function(chrome, $, mdash, graph) {
   "use strict";
 
   /**
-   * Start: Verifica se há alguma sincronização
+   * Start: Verifica se há alguma sincronização,
+   * se existir mostra o gráfico padrão
    */
    
   chrome.storage.local.get({
     sync: false,
   }, function(items) {
-    if (!items.sync) {
-      if($("#card-message-sync").length) {
-        $(".mdl-card").hide();
-        $("#card-message-sync").show();
-      }
+    if (items.sync) {
+      graph1();
+    } else {
+      $(".mdl-card").hide();
+      $("#card-message-sync").show();
     }
   });
+  
+  var graph1 = function() {
+    chrome.storage.local.get({
+      data: "",
+      sync: false
+    }, function(items) {
+      if (items.sync && items.data !== "") {
+        //Chama a visualização da tela apropriada
+        console.log("listOfActions");
+        var listOfActions = mdash.listOfActions(items.data);
+        console.log(listOfActions);
+        
+        $(".mdl-card").hide();
+        $(".mdl-card__title-text", "#card-graph").html("Ações");
+        $("#card-graph > .mdl-card__supporting-text").html("");
+  
+        graph.Bubble({
+          data: listOfActions,
+          context: "#card-graph > .mdl-card__supporting-text",
+          diameter: 400
+        });
+  
+        $("#card-graph").show();
+      }
+    });    
+  };
+  
+  var graph2 = function() {
+    chrome.storage.local.get({
+      data: "",
+      sync: false
+    }, function(items) {
+      if (items.sync && items.data !== "") {
+        //Chama a visualização da tela apropriada
+        console.log("listOfUsers");
+        var listOfUsers = mdash.listOfUsers(items.data);
+        console.log(listOfUsers);
+        
+        $(".mdl-card").hide();
+        $(".mdl-card__title-text", "#card-graph").html("Usuários e interações");
+        $("#card-graph > .mdl-card__supporting-text").html("");
+  
+        graph.Bar({
+          data: listOfUsers,
+          context: "#card-graph > .mdl-card__supporting-text",
+          width: 400
+        });
+  
+        $("#card-graph").show();
+      }
+    });    
+  };
 
   /**
    * Global: Em todas as páginas
@@ -46,7 +99,8 @@
               sync: true
             });
             
-            location.reload();
+            $("#spinner").hide();
+            graph1();
           },
           fail: function(params) {
             $("#spinner").hide();
@@ -80,4 +134,18 @@
       }
   });
   
-})(this.chrome, this.jQuery, this.mdash);
+  var drawer = $(".mdl-layout__drawer")[0];
+  
+  $("a", drawer).click(function() {
+    drawer.classList.toggle("is-visible");
+  });
+  
+  $(".btn-graph-1").click(function() {
+    graph1();
+  });
+  
+  $(".btn-graph-2").click(function() {
+    graph2();
+  });
+  
+})(this.chrome, this.jQuery, this.mdash, this.graph);
